4eb6961d3075a059a372a8947d8d7f02588150a2
bahn <dontsendloveletters@gmail.com>
2023-06-29 01:19:08 +0900

    Feat: 게스트 및 기타 투표인원 추가 크롤링

--- scraper.js
+++ scraper.js
@@ -49,8 +49,18 @@ const db = admin.database();
   })
   console.log(`Found ${modals} schedules.`)
 
+  //우리가 원하는 그것!!
   let participants = []
-  let dataTarget = '#wrap > div.layerContainerView > div > section > div > div:nth-child(1) > div > div > div.scheduleMain > div.scheduleRsvpArea > ul > li:nth-child(1) > label:nth-child(2) > span > span'
+  let hosts = []
+
+  //참석자
+  const dataTarget = '#wrap > div.layerContainerView > div > section > div > div:nth-child(1) > div > div > div.scheduleMain > div.scheduleRsvpArea > ul > li:nth-child(1) > label:nth-child(2) > span > span'
+
+  //2번째 라인. 참석 대기 or 불참석
+  const dataTargetWait = '#wrap > div.layerContainerView > div > section > div > div:nth-child(1) > div > div > div.scheduleMain > div.scheduleRsvpArea > ul > li.uTableList._pendingAttendArea > label:nth-child(2) > span > strong'
+
+  //벙주
+  const dataTargetHost = '#wrap > div.layerContainerView > div > section > div > div:nth-child(1) > div > div > div.scheduleHead._scheduleHead > div.contWrap > div > span.hostName'
 
   // 각 일정 모달을 열어서 참가자 명단을 크롤링
   for(let i = 1; i <= modals; i++) {
@@ -60,18 +70,60 @@ const db = admin.database();
     for(let j = 1; j <= contents; j++){
       await page.click(`#content > section > div.scheduleList.gContentCardShadow > ul > li:nth-child(${i}) > span > a:nth-child(${j})`)
       try {
-        await page.waitForSelector(dataTarget, { timeout: 1000 }); // timeout 옵션을 추가하여 짧은 시간 내에 찾지 못하면 넘어감
-    } catch (error) {
+        await page.waitForSelector(dataTarget, { timeout: 1000 }) // timeout 옵션을 추가하여 짧은 시간 내에 찾지 못하면 넘어감
+        await page.waitForSelector(dataTargetHost, { timeout: 1000 }) // timeout 옵션을 추가하여 짧은 시간 내에 찾지 못하면 넘어감
+      } catch (error) {
         console.log('dataTarget not found, skipping')
         await page.keyboard.press('Escape')
         continue // 해당 요소가 발견되지 않으면 다음 반복으로 건너뜀
-    }
+      } 
+
+      //참석자 명단 찾기
       const names = await page.evaluate((dataTarget) => {
         const element = document.querySelector(dataTarget)
         return element ? element.textContent : null
       }, dataTarget)
 
+      //벙주 찾기
+      const host = await page.evaluate((dataTargetHost) => {
+        const element = document.querySelector(dataTargetHost)
+        return element ? element.textContent : null
+      }, dataTargetHost)
+      
+      //참석 외의 추가 투표란 멤버 찾기
+      for(let k = 4; k <= 5; k++){
+
+        //참석 대기가 있을 경우 k+1을 사용
+        let n = k
+
+        //두번째 라인. 참석 대기 or 불참석임
+        const secondLine = await page.evaluate((dataTargetWait) => {
+          const element = document.querySelector(dataTargetWait)
+          return element ? element.textContent : null
+        }, dataTargetWait)
+
+        try {
+          //두번째 라인이 '참석 대기'면, 즉 게스트라인이 5번째부터 시작하면 n++부터 찾음.
+          if(secondLine === '참석 대기') n++
+          const getDataTargetSecond = (i) => `#wrap > div.layerContainerView > div > section > div > div:nth-child(1) > div > div > div.scheduleMain > div.scheduleRsvpArea > ul > li:nth-child(${i}) > label:nth-child(2) > span > span`
+          await page.waitForSelector(getDataTargetSecond(n), {timeout: 100})
+        } catch (error) {
+          break //해당 요소가 발견되지 않으면 추가투표란 찾기 종료
+        }
+
+        //추가 게스트 참석자
+        const restNames = await page.evaluate((n) => {
+          const getDataTargetSecond = (i) => `#wrap > div.layerContainerView > div > section > div > div:nth-child(1) > div > div > div.scheduleMain > div.scheduleRsvpArea > ul > li:nth-child(${i}) > label:nth-child(2) > span > span`
+
+          const element = document.querySelector(getDataTargetSecond(n))
+          return element ? element.textContent : null
+        }, n)
+        console.log('추가 게스트 발견')
+        participants = restNames ? participants.concat(restNames) : participants
+
+      }
       participants = names ? participants.concat(names) : participants
+      hosts = host ? hosts.concat(host) : hosts
       // 모달 닫기
       await page.keyboard.press('Escape')
     }
@@ -80,6 +132,8 @@ const db = admin.database();
   // 파이어베이스에 결과를 저장
   const ref = db.ref(`bandschedule/${month}month`)
   await ref.set(participants)
+  const refHost = db.ref(`hostList/${month}month`)
+  await refHost.set(hosts)
   console.log(`Data for schedule_${month} saved to Firebase.`)
   participants = [] // 다음 일정을 위해 참가자 배열 초기화
   await page.click(`#content > section > div.calendarViewWrap.gContentCardShadow > div:nth-child(1) > div.calendarHeader > div.month > button.prev._btnPrev`)
@@ -93,23 +147,27 @@ const db = admin.database();
   }
 })()
 
-
 //데이터 가공
 async function processFirebaseData () {
   try {
     // Firebase에서 데이터 가져오기
     const bandschedule = await db.ref('bandschedule').get()
     const memberList = await db.ref('memberList').get()
+    const hostList = await db.ref('hostList').get()
     const scheduleData = bandschedule.val()
     const memberListData = memberList.val()
+    const hostListData = hostList.val()
 
     let modifiedMemberList = memberListData
 
     // 데이터 가공 및 수정 작업
     let factoring = [{},{},{},{},{},{},{},{},{},{},{},{}]
+    let factoringHost = [{},{},{},{},{},{},{},{},{},{},{},{}]
     let users = {}
+    let usersHost = {}
 
     //1차 가공 - 월별로 정리
+    //벙 참석
     Object.values(scheduleData).forEach((schedule, i) => {
       schedule.forEach((list) => {
         list.split(',').forEach((name) => {
@@ -118,8 +176,18 @@ async function processFirebaseData () {
         })
       })
     })
+    //벙 개설
+    Object.values(hostListData).forEach((schedule, i) => {
+      schedule.forEach((list) => {
+        list.split(',').forEach((name) => {
+          let trimname = name.trim()
+          factoringHost[i][trimname] = (factoringHost[i][trimname] || 0) + 1
+        })
+      })
+    })
 
     //2차 가공 - 유저별로 정리
+    //벙 참석
     factoring.forEach((month, i) => {
       Object.entries(month).forEach(([name, count]) => {
         if (!users[name]) {
@@ -129,19 +197,45 @@ async function processFirebaseData () {
         users[name]['total'] = (users[name]['total'] || 0 ) + count
       })
     })
+    //벙 개설
+    factoringHost.forEach((month, i) => {
+      Object.entries(month).forEach(([name, count]) => {
+        if (!usersHost[name]) {
+          usersHost[name] = {}
+        }
+        usersHost[name][(i+1) + 'month'] = count
+        usersHost[name]['total'] = (usersHost[name]['total'] || 0 ) + count
+      })
+    })
 
     //최종 데이터 바인딩
+    //벙 참여
     for (let [id, userObj] of Object.entries(memberListData)){
-      const currentUserData = users[userObj.name];
+      const currentUserData = users[userObj.name]
       if (!currentUserData) { // users[userObj.name]이 존재하지 않는지 확인
-          continue; // 현재 반복 건너뛰고 다음 반복으로 넘어감
+        continue; // 현재 반복 건너뛰고 다음 반복으로 넘어감
       }
       
       for(let [month, count] of Object.entries(currentUserData)) {
-          if (!modifiedMemberList[id]) {
-              modifiedMemberList[id] = {}
-          }
-          modifiedMemberList[id][month] = count;
+        if (!modifiedMemberList[id]) {
+          modifiedMemberList[id] = {}
+        }
+        modifiedMemberList[id][month] = count;
+      }
+    }
+
+    //벙 개설
+    for (let [id, userObj] of Object.entries(memberListData)){
+      const currentUserData = usersHost[userObj.name]
+      if (!currentUserData) { // users[userObj.name]이 존재하지 않는지 확인
+        continue; // 현재 반복 건너뛰고 다음 반복으로 넘어감
+      }
+      
+      for(let [month, count] of Object.entries(currentUserData)) {
+        if (!modifiedMemberList[id]) {
+          modifiedMemberList[id] = {}
+        }
+        modifiedMemberList[id][`${month}Host`] = count;
       }
     }
     
