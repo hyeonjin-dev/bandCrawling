b76f143e7d181e180033b275874a605bef0318a0
Hyeon JIN <50274369+hyeonjin-dev@users.noreply.github.com>
2023-07-17 08:57:26 +0000

    몰라

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
   },
-  externals: {
-    'chrome-aws-lambda': 'chrome-aws-lambda',
-    'puppeteer-core': 'puppeteer-core'
-  }
 };
